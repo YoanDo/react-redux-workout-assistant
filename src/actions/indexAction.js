0fb773b625a42f3d80edb88802efc3755c737d27
Y <y.n.dorny@gmail.com>
2018-11-13 22:03:25 +0100

    lint

--- src/actions/indexAction.js
+++ src/actions/indexAction.js
@@ -1,17 +1,14 @@
 export const actionTypes = {
-    toggle_run: "TOGGLE_RUN",
-    update_value: "UPDATE_VALUE"
-}
+  toggle_run: 'TOGGLE_RUN',
+  update_value: 'UPDATE_VALUE',
+};
 
-export const toggle_run = () => {
-    return{
-        type: actionTypes.toggle_run
-    }
-}
+export const toggle_run = () => ({
+  type: actionTypes.toggle_run,
+});
 
-export const update_value = (label, new_value) => (dispatch) => {
-    return dispatch({
-      type: actionTypes.update_value,
-      label, new_value
-    })
-}
+export const update_value = (label, new_value) => dispatch => dispatch({
+  type: actionTypes.update_value,
+  label,
+  new_value,
+});
--- src/reducers/indexReducer.js
+++ src/reducers/indexReducer.js
@@ -1,26 +1,26 @@
-import {actionTypes} from '../actions/indexAction'
+import { actionTypes } from '../actions/indexAction';
 
 const defaultState = {
-    time:5,
-    serie:1,
-    rest:5,
-    loop:1,
-    running: false,
+  time: 5,
+  serie: 1,
+  rest: 5,
+  loop: 1,
+  running: false,
 };
 
 export default (state = defaultState, action) => {
-    switch (action.type){
-        case actionTypes.toggle_run:
-          return {
-              ...state,
-              running: !state.running,
-          }
-        case actionTypes.update_value:
-          return {
-            ...state,
-            [action.label]: action.new_value
-        }
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case actionTypes.toggle_run:
+      return {
+        ...state,
+        running: !state.running,
+      };
+    case actionTypes.update_value:
+      return {
+        ...state,
+        [action.label]: action.new_value,
+      };
+    default:
+      return state;
+  }
 };
