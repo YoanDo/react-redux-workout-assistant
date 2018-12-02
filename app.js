16cbe4c45408ba5f8b6214ea2b72e468918d1302
Y <y.n.dorny@gmail.com>
2018-12-02 19:59:40 +0100

    app & procfile

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const http = require('http');
+const path = require('path');
+let app = express();
+
+// app.use(express.static(path.join(__dirname, 'build')));
+
+// use(connect.static(__dirname + '/public'));
+
+app.use(express.static('public'));
+
+const port = process.env.PORT || '8080';
+app.set('port', port);
+const server = http.createServer(app);
+server.listen(port, () => console.log(`Running on localhost:${port}`));
